893eb135bc09da9b4b84d21ce50cac1294badbb2
Ralond <mrralond@gmail.com>
2025-10-23 08:25:41 +1000

    23.10.25

--- intermediate/mod_13/lesson_1_3.cpp
+++ intermediate/mod_13/lesson_1_3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-// НЕ ДОДЕЛАЛ
+
 int main(){
     int n;
     cout << "Enter N:"; cin >> n;
@@ -9,12 +9,17 @@ int main(){
     vector<int> vec(n);
     for (int i = 0; i < n; i++) cin >> vec[i];
 
-    int minOne = vec[0], minTwo = vec[0];
-    for(int i = 1; i < n; i++){
-        if (minTwo > vec[i]){
+    int minOne = INT_MAX, minTwo = INT_MAX;
+    for(int i = 0; i < n; i++){
+        if (minOne > vec[i]){
+            minTwo = minOne;
             minOne = vec[i];
-            if (minTwo > minOne) minTwo = minOne;
-        }
+        } else if (minTwo > vec[i] && vec[i] != minOne) minTwo = vec[i];
     }
-    cout << "Heve: " << minTwo << endl;
+
+    if (minTwo == INT_MAX) {
+        cout << "All elements are equal or second minimum not found" << endl;
+    } else {
+        cout << "Second minimum: " << minTwo << endl;
+    }
 }
---
+++ intermediate/mod_13/lesson_2_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> queue = {52, 23, 44, 34, 11};
+
+    int num = 0;
+    while (num != 1){
+        
+    }
+}
