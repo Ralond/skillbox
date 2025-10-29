3e8d13dd12f1f2db6a3ef83cb89e6276677ef0df
Ralond <mrralond@gmail.com>
2025-10-29 21:25:31 +1000

    29.10.25 done!

--- final/mod_14/task_6.cpp
+++ final/mod_14/task_6.cpp
@@ -5,19 +5,13 @@ int main(){
     int matrix[5][5];
     
 
-    int count = 0;
+    int number = 0;
     for (int i = 0; i < 5; i++){
         for(int j = 0; j < 5; j++){
-            matrix[i][j-count*(1-2*(i%2))] = count;
-            count++;
+            matrix[i][abs(j-4*(i%2))] = number;
+            number++;
         }
-        //1 - 2*i%2 = будет выдавать 1 или -1 в зависимости от четности строки.
-        //если строка нечетная - нам надо инициализировать в обратном порядке.
-        //           1 - 2 - 3 - 4
-        //т.е. будет 8 - 7 - 6 - 5
     }
-
-
     for (int i = 0; i < 5; i++){
         int count = 0;
         for(int j = 0; j < 5; j++){
