542c3db9fbb1df184f3b640cfd4ff2f96cb65d7b
Ralond <mrralond@gmail.com>
2025-11-03 00:18:33 +1000

    idk

--- final/mod_15/task_1.cpp
+++ final/mod_15/task_1.cpp
@@ -4,24 +4,19 @@ using namespace std;
 
 int main(){
     vector<int> vec = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-    vector<int> summ(vec.size(),0);
-    int start = 0, end = 0, fin = vec.size()-1, sum = 0, temp;
+    int start = 0, end = 0, fin = vec.size()-1, sum = 0;
 
-    for (int i = 0; i < vec.size(); i++){
-        sum += vec[i];
-    }
-    
-    for (int i = 0; i < fin; i++){
-        temp = 0;
-        for(int j = i; j < fin; j++){
+    for (int i = 0; i < vec.size()-1; i++){
+        int temp = 0;
+        for(int j = vec.size()-1; j > i; j--){
             temp += vec[j];
+            if (temp > sum) end = j;
         }
+        cout << i << " " << sum << " " << temp << " " << end << endl;
         if (temp > sum){
             start = i;
-            end = fin;
             sum = temp;
         }
-        fin--;
     }
 
     cout << start << ", " << end;
