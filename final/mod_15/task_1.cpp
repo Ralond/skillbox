2e7d6ebe024d077244b596178c64b49063570255
ralond <mrralomd@gmail.com>
2025-11-03 11:30:02 +1000

    task_1 make O(n^3)

--- final/mod_15/task_1.cpp
+++ final/mod_15/task_1.cpp
@@ -4,21 +4,22 @@ using namespace std;
 
 int main(){
     vector<int> vec = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-    int start = 0, end = 0, fin = vec.size()-1, sum = 0;
+    int start = 0, end = 0, sum = vec[0];
 
     for (int i = 0; i < vec.size()-1; i++){
-        int temp = 0;
-        for(int j = vec.size()-1; j > i; j--){
-            temp += vec[j];
-            if (temp > sum) end = j;
-        }
-        cout << i << " " << sum << " " << temp << " " << end << endl;
-        if (temp > sum){
-            start = i;
-            sum = temp;
+        int fin = vec.size()-1;
+        while (fin > i){
+            int temp = 0;
+            for(int j = fin; j >= i; j--){
+                temp += vec[j];
+            }
+            if (temp > sum){
+                start = i;
+                sum = temp;
+                end = fin;
+            }
+            fin--;
         }
     }
-
     cout << start << ", " << end;
-
 }
