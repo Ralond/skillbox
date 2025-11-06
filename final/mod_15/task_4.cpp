86a0ed0b306dac926f53d490a42835c5ad3d51b2
Ralond <mrralond@gmail.com>
2025-11-06 22:59:22 +1000

    15_task_4. My var.

--- final/mod_15/task_4.cpp
+++ final/mod_15/task_4.cpp
@@ -2,8 +2,38 @@
 #include <vector>
 using namespace std;
 
+void sortMod(vector<int> vec){
+    int ind = 0;
+    for(int i=1; i < vec.size();i++){
+        if (abs(vec[i-1]) < abs(vec[i]) && abs(vec[i-1]) < abs(vec[ind])){
+            ind = i-1;
+        } else if (abs(vec[i]) < abs(ind)) ind = i;
+    }
+    cout << vec[ind] << " ";
+
+    int left = ind-1, right = ind+1;
+    int lol = vec.size();
+    while (left >= 0 && right < vec.size()){
+        if (abs(vec[left]) < abs(vec[right])){
+            cout << vec[left] << " ";
+            left--;
+        } else{ 
+            cout << vec[right] << " ";
+            right++;
+        }
+    }
+    if (left < 0){
+        for (int i = right; i < vec.size(); i++){
+            cout << vec[i] << " ";
+        }
+    } else if (right >= vec.size()){
+        for (int i = left; i >= 0; i--){
+            cout << vec[i] << " ";
+        }
+    }
+}
 
 int main(){
-    vector<int> vec;
-
+    vector<int> vec = {-100, -50, -5, 1, 10, 15};
+    sortMod(vec);
 }
