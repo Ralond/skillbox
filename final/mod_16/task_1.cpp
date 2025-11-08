9548e48cd787902acd5c6f0e9191074bd8bff8ad
ralond <mrralomd@gmail.com>
2025-11-08 17:19:05 +1000

    corr

--- final/mod_16/task_1.cpp
+++ final/mod_16/task_1.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 
 int main(){
-double speed = 0, delta;
-char buffer[5];
+    double speed = 0, delta;
+    char buffer[5];
 
-do {
-    cout << "Speed delta: ";
-    cin >> delta;
+    do {
+        cout << "Speed delta: ";
+        cin >> delta;
 
-    speed += delta;
+        speed += delta;
 
-    if (speed > 150) speed = 150;
-    
-    sprintf(buffer, "%.1f", speed);
-    cout << "Speed: " << buffer << endl;
-} while (speed > 0.01);
+        if (speed > 150) speed = 150;
+        
+        sprintf(buffer, "%.1f", speed);
+        cout << "Speed: " << buffer << endl;
+    } while (speed > 0.01);
 }
