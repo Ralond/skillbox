c3c903f56890c013c3b60d869f37f42c326fdca0
ralond <mrralomd@gmail.com>
2025-11-10 12:57:45 +1000

    10.11.25. 16_task_5. Done

--- final/mod_16/task_5.cpp
+++ final/mod_16/task_5.cpp
@@ -19,21 +19,30 @@ void printTime (int hours, int min){
     cout << min << endl;
 }
 
-void chekAutomats(unsigned char one, unsigned char buffer){
+void chekAutomats(unsigned char now, unsigned char old){
     //проверка предыдущего состояния, если состояние изменено - вывод сообщения.
+    unsigned char changes = now ^ old;
+    const char* names[] = {"All power", "Sockets", "Light inside", "Light outside", "Heating", "Hot water", "Conditioner"};
+    int mask[] = {allPower, sockets, lightIn, lightOut, heating, hotWater, conditioner};
+    if (changes) cout << "----\n";
+    for (int i =0; i < 7; i++){
+        if (changes & mask[i]){
+            cout << names[i] << ((now & mask[i])? " On": " Off") << endl;
+        }
+    }
 }
 
 int main(){
     int tempOutside, tempIn, lightTemp = 5000, hours = 0, min = 0;
     string mov, lights;
-    unsigned char switches_state = 0, switches_buff;
+    unsigned char switches_state = 0, switches_buff = 0;
 
     for (int i = 0; i < 48; i++){ //симуляция двух суток
         cout << "Time: ";
         printTime(hours, min);
         string enter;
         cout << "Temperature inside, temperature outside, movement, lights: \n";
-        cin >> enter;
+        getline(cin, enter);
         stringstream buffer(enter);
         buffer >> tempIn >> tempOutside >> mov >> lights;
 
@@ -44,7 +53,7 @@ int main(){
         }
         if ((hours > 16 || hours < 5) && mov == "yes"){
             switches_state |= lightOut;
-        } else if (hours >= 5 && hours <= 16){
+        } else if (mov == "no" || hours >= 5 && hours <= 16){
             switches_state &= ~lightOut;
         }
         if (tempIn < 22){
@@ -62,8 +71,17 @@ int main(){
         } else if (hours == 0){
             lightTemp = 5000;
         }
+        if (lights == "on"){
+            switches_state |= lightIn;
+        } else {
+            switches_state &= ~lightIn;
+        }
 
         chekAutomats(switches_state, switches_buff);
+        if (switches_state & lightIn){
+            cout << "Color temperature: " << lightTemp << "K" << endl;
+        }
+        cout << "------------------------------------\n";
         switches_buff = switches_state;
 
         hours++;
